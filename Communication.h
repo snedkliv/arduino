37a55e71693c7716edf9bed81f8470d7f29e02ce
snedkliv <snedkliv@gmail.com>
2014-10-26 16:14:02 +0100

    Create Communication.h

---
+++ Communication.h
@@ -0,0 +1,17 @@
+#ifndef Communication_h
+#define Communication_h
+
+
+class Communication
+{
+	public:
+		Communication();
+		
+		void UpdateMenuInputs();
+		void UpdateGameInputs();
+		
+		void GetPlayerStates();
+		void UpdateGameOutputs();
+};
+
+#endif
